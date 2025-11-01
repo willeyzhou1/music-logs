7e001d957c9156bfacf95c099e838f047e91df30
willeyzhou1 <willzhou000@gmail.com>
2025-10-31 23:08:01 -0700

    path changed for videos

--- src/assets/data/VideoData.jsx
+++ src/assets/data/VideoData.jsx
@@ -1,23 +1,23 @@
 const VideoData = [
   {
     "title": "City of Tears Theme from Hollow Knight",
-    "path": "/src/assets/videos/IMG_7917.mov"
+    "path": "/music-logs/src/assets/videos/IMG_7917.mov"
   },
   {
     "title": "SWEET/I THOUGHT YOU WANTED TO DANCE",
-    "path": "/src/assets/videos/IMG_8262.mov"
+    "path": "/music-logs/src/assets/videos/IMG_8262.mov"
   },
   {
     "title": "Real Thing - Pink Sweats & Tori Kelly",
-    "path": "/src/assets/videos/IMG_6543.mov"
+    "path": "/music-logs/src/assets/videos/IMG_6543.mov"
   },
   {
     "title": "Fun lil melody I came up with :D",
-    "path": "/src/assets/videos/IMG_6593.mov"
+    "path": "/music-logs/src/assets/videos/IMG_6593.mov"
   },
   {
     "title": "Chivalry Is Dead - Trevor Wesley",
-    "path": "/src/assets/videos/chivalry_is_dead.mov"
+    "path": "/music-logs/src/assets/videos/chivalry_is_dead.mov"
   },
 ]
 
--- vite.config.js
+++ vite.config.js
@@ -8,5 +8,5 @@ export default defineConfig({
     react(), 
     tailwindcss()
   ],
-  base: '/music-logs/'
+  base: "/music-logs/"
 })
