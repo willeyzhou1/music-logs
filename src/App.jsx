fe07bb8c7b9370dbeb120487b70a644928158bb7
willeyzhou1 <willzhou000@gmail.com>
2025-10-31 19:02:02 -0700

    cursor image made with framer motion + frutiger aero designs implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,40 @@
 import './App.css'
 import Header from './pages/Header'
 import Videos from './pages/Videos'
+import { motion } from "motion/react"
+import { useState, useEffect } from 'react'
+import dolphinGif from './assets/images/dolphins.gif'
 
 function App() {
+  function getMousePosition() {
+    const [mousePosition, setMousePosition] = useState({x: 0, y: 0})
+    
+    useEffect(() => {
+      const updatePosition = (e) => {
+        setMousePosition({ x: e.clientX, y: e.clientY});
+      };
+      window.addEventListener('mousemove', updatePosition)
 
+      return () => window.removeEventListener('mousemove', updatePosition);
+    }, []);
+
+    return mousePosition
+  }
+
+  const mousePosition = getMousePosition();
   return (
+    
     <div className="home-page">
-      <div className="bg-[url('/src/assets/frutiger-aero.webp')] bg-[length:100%_100%] bg-no-repeat min-h-screen text-[rgb(6,24,102)]">
+      <div className="bg-[url('/src/assets/images/frutiger-aero.webp')] bg-[length:100%_100%] bg-no-repeat min-h-screen text-[rgb(6,24,102)]">
+        <motion.img 
+        src={dolphinGif}
+        alt="Swimming Dolphin"
+        animate={{
+          x: mousePosition.x,
+          y: mousePosition.y - 150,
+        }}
+        style={{ position: 'fixed', pointerEvents: 'none', zIndex: 9999, filter: "drop-shadow(#60D5FF 0.7rem 0.7rem 7px)"}}
+      />
         <Header />
         <Videos />
       </div>
--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Video = ({title, path}) => {
   return (
-    <div className='block border-4 rounded-3xl mx-3 mt-6 pt-6 pb-6 bg-gradient-to-br from-sky-400 to-blue-200 max-w-xs min-w-xs'>
-      <h1 className="text-3xl text-center">{title}</h1>
-      <video className="mx-auto my-auto mt-4 border-4 border-blue" height="480" width="270" controls>
+    <div className='block border-4 border-blue-700 rounded-3xl mx-3 my-6 pt-6 pb-6 bg-gradient-to-br from-sky-400 to-blue-200 max-w-xs min-w-xs drop-shadow-[1.2rem_1.2rem_6px_rgba(0,0,0,0.35)]'>
+      <h1 className="text-3xl text-center w-[90%] mx-auto my-0">{title}</h1>
+      <video className="mx-auto my-auto mt-4 border-4 rounded-3xl border-blue-700" height="480" width="270" controls>
         <source src={path} />
       </video>
     </div>
