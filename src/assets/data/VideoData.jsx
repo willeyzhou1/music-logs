9d5f2bb6fc481c9b3541ffeaa95619550f867809
willeyzhou1 <willzhou000@gmail.com>
2025-10-31 23:34:55 -0700

    changed path

--- src/assets/data/VideoData.jsx
+++ src/assets/data/VideoData.jsx
@@ -1,23 +1,29 @@
+import CityOfTears from "../videos/IMG_7917.mov"
+import Sweet from "../videos/IMG_8262.mov"
+import RealThing from "../videos/IMG_6543.mov"
+import LilMelody from "../videos/IMG_6593.mov"
+import Chivalry from "../videos/chivalry_is_dead.mov"
+
 const VideoData = [
   {
     "title": "City of Tears Theme from Hollow Knight",
-    "path": "IMG_7917.mov"
+    "path": CityOfTears
   },
   {
     "title": "SWEET/I THOUGHT YOU WANTED TO DANCE",
-    "path": "IMG_8262.mov"
+    "path": Sweet
   },
   {
     "title": "Real Thing - Pink Sweats & Tori Kelly",
-    "path": "IMG_6543.mov"
+    "path": RealThing
   },
   {
     "title": "Fun lil melody I came up with :D",
-    "path": "IMG_6593.mov"
+    "path": LilMelody
   },
   {
     "title": "Chivalry Is Dead - Trevor Wesley",
-    "path": "chivalry_is_dead.mov"
+    "path": Chivalry
   },
 ]
 
