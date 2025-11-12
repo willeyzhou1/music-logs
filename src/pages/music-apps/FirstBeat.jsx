158e79501231fcb32c61340680d2f7f553d52e03
willeyzhou1 <willzhou000@gmail.com>
2025-11-11 18:01:17 -0800

    quick lil change

--- src/pages/music-apps/FirstBeat.jsx
+++ src/pages/music-apps/FirstBeat.jsx
@@ -3,18 +3,17 @@ import * as Tone from 'tone'
 
 export default function FirstBeat() {
   Tone.start().then(() => {
-    const reverb = new Tone.Reverb(0).toDestination()
     const sampler = new Tone.Sampler({
       urls: {
         "D2": "D2.mp3",
         "B1": "B1.mp3"
       },
       baseUrl: "https://tonejs.github.io/audio/casio/",
-    }).connect(reverb);
+    }).toDestination();
     Tone.loaded().then(() => {
       const now = Tone.now();
-      sampler.triggerAttackRelease("D2", "2n", now);
-      sampler.triggerAttackRelease("B1", "2n", now + 1);
+      sampler.triggerAttackRelease("D2", "1m", now);
+      sampler.triggerAttackRelease("B1", "1m", now + 1);
     })
   })
 }
