6769701512c7d247d32b1ffd2c09f8dc45e3151c
willeyzhou1 <willzhou000@gmail.com>
2025-11-12 12:33:20 -0800

    sound works again

--- src/pages/music-apps/MusicLanding.jsx
+++ src/pages/music-apps/MusicLanding.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
-import FirstBeat from './FirstBeat'
+import FirstBeat from './tonejs-beats/FirstBeat'
+import * as Tone from 'tone'
 
 export default function MusicLanding() {
+  function PauseSound() {
+    Tone.stop();
+  }
   return (
-    <div className='w-full flex justify-center flex-wrap'>
-      <button className="text-3xl mx-12 my-6 bg-yellow-300 border-4 p-2 rounded-2xl w-[40%] h-18" onClick={FirstBeat}>First Beat</button>
-      <button className="text-3xl mx-12 my-6 bg-yellow-300 border-4 p-2 rounded-2xl w-[40%] h-18" onClick={FirstBeat}>First Beat</button>
-      <button className="text-3xl mx-12 my-6 bg-yellow-300 border-4 p-2 rounded-2xl w-[40%] h-18" onClick={FirstBeat}>First Beat</button>
-      <button className="text-3xl mx-12 my-6 bg-yellow-300 border-4 p-2 rounded-2xl w-[40%] h-18" onClick={FirstBeat}>First Beat</button>
-
+    <div className='block'>
+      <button className="text-3xl mx-auto my-6 bg-yellow-300 border-3 p-2 rounded-2xl w-[40%] h-18 block " onClick={PauseSound}>First Sound</button>
+        <div className='w-full flex justify-center flex-wrap'>
+        <button className="text-3xl mx-12 my-6 bg-yellow-300 border-3 p-2 rounded-2xl w-[40%] h-18" onClick={FirstBeat}>First Sound</button>
+        <button className="text-3xl mx-12 my-6 bg-yellow-300 border-3 p-2 rounded-2xl w-[40%] h-18" onClick={FirstBeat}>First Beat</button>
+        <button className="text-3xl mx-12 my-6 bg-yellow-300 border-3 p-2 rounded-2xl w-[40%] h-18" onClick={FirstBeat}>First Beat</button>
+        <button className="text-3xl mx-12 my-6 bg-yellow-300 border-3 p-2 rounded-2xl w-[40%] h-18" onClick={FirstBeat}>First Beat</button>
+    </div>
     </div>
   )
 }
