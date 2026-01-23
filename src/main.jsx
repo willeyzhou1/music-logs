6bda3d4dbaddfb628f683ce343f937a7926fc86f
willeyzhou1 <willzhou000@gmail.com>
2026-01-22 18:37:32 -0800

    lmfao nvm

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/music-logs">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>,
